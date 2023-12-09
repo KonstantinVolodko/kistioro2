52441e1d84f409ceccb66d1691c6448b17470a27
Vadzim <vadim.dolgiy.2003@mail.ru>
2023-12-09 19:25:53 +0300

    add new page and components

---
+++ src/components/InputChangeProfile.vue
@@ -0,0 +1,71 @@
+<script setup>
+    const props = defineProps({
+        label: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+    })
+</script>
+
+<template>
+    <div class="inputGroup">
+        <input :type="type" autocomplete="off">
+        <label for="name">{{label}}</label>
+    </div>
+</template>
+
+<style lang="scss">
+.inputGroup {
+    font-family: Gilroy;
+    color: var(--Black, #2C2C2C);
+    leading-trim: both;
+
+    text-edge: cap;
+    font-family: Gilroy;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: 0.0016rem;
+    position: relative;
+}
+
+.inputGroup input {
+  outline: none;
+  border: .1rem solid rgba(44, 44, 44, 0.30);
+  background-color: transparent;
+  border-radius: .8rem;
+  width: 42.5rem;
+  height: 3.9rem;
+  padding-left: 1.1rem;
+  color: var(--Black, #2C2C2C);
+}
+
+.inputGroup label {
+  font-size: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  padding: 0.8em;
+  margin-left: 0.5em;
+  pointer-events: none;
+  transition: all 0.3s ease;
+  color: rgba(44, 44, 44, 0.30);
+}
+
+.inputGroup :is(input:focus, input:valid)~label {
+  transform: translateY(-50%) scale(.9);
+  margin: 0em;
+  margin-left: 1.1rem;
+  padding: 0rem;
+  background-color: white;
+}
+
+.inputGroup :is(input:focus, input:valid) {
+  border-color: rgba(44, 44, 44, 0.30);
+}
+</style>
---
+++ src/components/PortfolioChangePhoto.vue
@@ -0,0 +1,73 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="portfolio-master-card">
+        <div class="portfolio-master-card__img">
+            <img src="../assets/images/portfolioForClient/masterPhoto.jpg" alt="#">
+        </div>
+        <div class="portfolio-photo__change-img">
+            <input id="change-img__file" type="file" class="change-img__file" multiple>
+            <label class="change-img__true-file" for="change-img__file">
+                <span>
+                    <svg class="change-file">
+                        <use href="#change-file"></use>
+                    </svg>
+                </span>
+                <span>
+                    Загрузить новое фото
+                </span>
+            </label>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.portfolio-master-card {
+    display: flex;
+    gap: 1.6rem;
+    margin-top: 1.6rem;
+}
+
+.portfolio-master-card__img {
+    width: 12rem;
+    height: 12rem;
+    border-radius: 50%;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+}
+.portfolio-photo__change-img {
+    display: flex;
+    align-items: center;
+}
+.change-file {
+    width: 2rem;
+    height: 2rem;
+}
+.change-img__file {
+    display: none;
+}
+.change-img__true-file {
+    display: flex;
+    align-items: center;
+    gap: 0.6rem;
+    cursor: pointer;
+}
+.change-img__true-file span:last-child {
+    color: #000;
+    leading-trim: both;
+    text-edge: cap;
+    font-family: Gilroy;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 2.8rem; /* 175% */
+    text-decoration-line: underline;
+}
+</style>
