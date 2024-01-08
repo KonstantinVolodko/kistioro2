f1ab3c3a7291c17a3f1b0b4c82e727feabc7fe67
Vadzim <vadim.dolgiy.2003@mail.ru>
2024-01-08 23:08:16 +0300

    add new pages & components

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import PortfolioForClient from '../pages/PortfolioForClient.vue'
 import settingsAccount from "../pages/settingsAccount.vue"
 import PortfolioForMaster from "../pages/PortfolioForMaster.vue"
 import HistoryOfOrders from '../pages/HistoryOfOrders.vue'
+import settingsAccountIsp from '../pages/settingsAccountIsp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: '/HistoryOfOrders',
       name: 'HistoryOfOrders',
       component: HistoryOfOrders
+    },
+    {
+      path: '/settingsAccountIsp',
+      name: 'settingsAccountIsp',
+      component: settingsAccountIsp
     },
     
   ]
